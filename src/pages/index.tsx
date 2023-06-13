bdb612aa6c65b420dd4e671492e097730daefa33
Ke1too <s1f102301730@iniad.org>
2023-06-13 19:10:20 +0900

    オセロレベル１

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,16 @@ import styles from './index.module.css';
 
 const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
+  const directions = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [0, -1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+  ];
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -16,12 +26,46 @@ const Home = () => {
   const onClick = (x: number, y: number) => {
     console.log(x, y);
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-    if (board[y + 1] !== undefined && board[y + 1][x] !== 0 && board[y + 1][x] !== turnColor) {
-      newBoard[y][x] = turnColor;
-      setTurnColor(3 - turnColor);
+
+    let k = false;
+    if (board[y][x] === 0) {
+      for (const [y2, x2] of directions) {
+        if (
+          board[y + y2] !== undefined &&
+          board[y + y2][x + x2] !== undefined &&
+          board[y + y2][x + x2] === 3 - turnColor //逆の色の石が付近に置けるかどうか
+        ) {
+          for (let i = 1; i < 8; i++) {
+            if (
+              board[y + y2 * i] !== undefined &&
+              board[y + y2 * i][x + x2 * i] !== undefined &&
+              board[y + y2 * i][x + x2 * i] === turnColor //升目の端から端まで
+            ) {
+              let a = 0;
+              for (let i2 = 1; i2 <= i; i2++) {
+                if (board[y + y2 * i2][x + x2 * i2] !== 0) {
+                  a++; //???
+                }
+              }
+              if (a === i) {
+                newBoard[y][x] = turnColor;
+                k = true;
+                for (let i2 = 1; i2 < i; i2++) {
+                  newBoard[y + y2 * i2][x + x2 * i2] = turnColor;
+                }
+              }
+              break;
+            }
+          }
+        }
+      }
+      if (k) {
+        setTurnColor(3 - turnColor);
+        setBoard(newBoard);
+      }
     }
-    setBoard(newBoard);
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.board}>
