954433159b83a4af2648b643120cb7ee225c78c7
Ke1too <Ke1too@users.noreply.github.com>
2023-06-29 12:57:34 +0000

    deploy: a6870ede008b1c01f68cb133b4340851279d9903

--- _next/static/IsXvYYAL19Qqo0riydj27/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/90ee6ce47577c444.css","static/chunks/pages/index-2210fa91ed006597.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
